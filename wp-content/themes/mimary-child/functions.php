e0eb31fefd8a4f8bad5f0d3d10758f7ca99b1fca
Eman Khalifa <emankhalifa@theappconcept.com>
2017-07-13 11:59:58 +0200

    Adding Security Configurations in htaccess, wp-config and functions.php

--- wp-content/themes/mimary-child/functions.php
+++ wp-content/themes/mimary-child/functions.php
@@ -1,5 +1,12 @@
 <?php
-/**
- * Your code here.
- *
- */
+
+ 
+function remove_version() {
+  return '';
+}
+add_filter('the_generator', 'remove_version');
+
+function wrong_login() {
+  return 'Wrong username or password.';
+}
+add_filter('login_errors', 'wrong_login');
