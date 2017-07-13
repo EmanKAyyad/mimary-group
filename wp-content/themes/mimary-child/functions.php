9fbfd9881eb8bf1783385bf3b630fcecbeccd765
Eman Khalifa <emankhalifa@theappconcept.com>
2017-07-13 12:10:31 +0200

    adding css and js files

--- wp-content/themes/mimary-child/functions.php
+++ wp-content/themes/mimary-child/functions.php
@@ -1,6 +1,19 @@
 <?php
 
- 
+add_action( 'wp_enqueue_scripts', 'themename_enqueue_parent_styles' ); 
+	 
+function themename_enqueue_parent_styles() { 
+	 
+	if (is_rtl()) { 
+    	wp_enqueue_style( 'custom-child-ar-style', get_stylesheet_directory_uri(). '/css/customar.css' );
+    	wp_enqueue_script( 'myscript-ar', get_stylesheet_directory_uri() . '/js/customar.js'); 
+	} 
+    	wp_enqueue_style( 'custom-child-style', get_stylesheet_directory_uri(). '/css/custom.css' ); 
+    	wp_enqueue_script( 'myscript', get_stylesheet_directory_uri() . '/js/custom.js');  	 
+}
+
+
+
 function remove_version() {
   return '';
 }
